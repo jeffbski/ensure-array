7c6a9b8b59706ea5913a14ec3802c15707be197e
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-03-24 18:27:24 -0500

    switch from tap to mocha and use universal module def so will be compatible with node.js and the browser

--- ensure-array.js
+++ ensure-array.js
@@ -1,10 +1,30 @@
-'use strict';
+(function (root, factory) {
+  /*global define:true */
 
-module.exports = function array(a, b, n) {
-  if (arguments.length === 0) return [];            //no args, ret []
-  if (arguments.length === 1) {                     //single argument
-    if (a === undefined || a === null) return [];   //  undefined or null, ret []
-    if (Array.isArray(a)) return a;                 //  isArray, return it
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else {
+        // Browser globals
+        root.ensureArray = factory();
+    }
+}(this, function () {
+  'use strict';
+
+  function ensureArray(a, b, n) {
+    if (arguments.length === 0) return [];            //no args, ret []
+    if (arguments.length === 1) {                     //single argument
+      if (a === undefined || a === null) return [];   //  undefined or null, ret []
+      if (Array.isArray(a)) return a;                 //  isArray, return it
+    }
+    return Array.prototype.slice.call(arguments);     //return array with copy of all arguments
   }
-  return Array.prototype.slice.call(arguments);     //return array with copy of all arguments
-};
+
+  return ensureArray;
+}));
+
